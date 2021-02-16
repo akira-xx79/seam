f80b078e369bcb2a2290767e0a120f836d96d322
akira <akiraxx79xx@gemail.com>
2021-02-16 19:00:19 +0900

    top画面の制作

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 @section('content')
-<div class="jumbotron-fluid">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-</div>
+    <div id="top" class="jumbotron jumbotron-fluid p-0">
+      <a href="#" class="btn btn-danger btn-lg px-4">お問い合わせ</a>
+    </div>
+
+        <div class="col-sm-12">
+         <img class="d-block mx-auto" src="storage/seam問題画像.svg" alt="問題画像">
+        </div>
+
 @endsection
